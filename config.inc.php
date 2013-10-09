16e8b35efd5b99ead4b3c1c857a65b3fd55aa56f
laguirre70 <leonelaguirre@hotmail.com>
2013-10-09 16:40:05 -0700

    initial commit

---
+++ config.inc.php
@@ -0,0 +1,17 @@
+<?php
+# config.inc.php - SiT! Config file generated automatically by setup.php on Wed, 06 Oct 2010 13:27:05 -0700
+
+# Web Path
+# The path to SiT! from the browsers perspective with a trailing slash. e.g. /sit/
+$CONFIG['application_webpath'] = '/sit-3.65/';
+
+# MySQL Database Username
+$CONFIG['db_username'] = 'root';
+
+# MySQL Database Password
+$CONFIG['db_password'] = '6@1dk3y';
+
+# Attachment Filesystem Path
+# The full absolute file system path to a directory to store attachments in (with a trailing slash). This directory should be writable
+$CONFIG['attachment_fspath'] = 'C:\\Inetpub\\wwwroot\\sit-3.62\\attachments-bbf5c2c761db782a8630d631279f7e13\\';
+
