7deb9372bb0b3b718537fcd4573ebbc5971aa78a
laguirre70 <leonelaguirre@hotmail.com>
2013-10-10 16:34:47 -0700

    Update config.inc.php
    Added azure db user and pwd, and change app paths

--- config.inc.php
+++ config.inc.php
@@ -3,15 +3,15 @@
 
 # Web Path
 # The path to SiT! from the browsers perspective with a trailing slash. e.g. /sit/
-$CONFIG['application_webpath'] = '/sit-3.65/';
+$CONFIG['application_webpath'] = '/';
 
 # MySQL Database Username
-$CONFIG['db_username'] = 'root';
+$CONFIG['db_username'] = 'bc5dce6c699515';
 
 # MySQL Database Password
-$CONFIG['db_password'] = '6@1dk3y';
+$CONFIG['db_password'] = 'eb0f293e';
 
 # Attachment Filesystem Path
 # The full absolute file system path to a directory to store attachments in (with a trailing slash). This directory should be writable
-$CONFIG['attachment_fspath'] = 'C:\\Inetpub\\wwwroot\\sit-3.62\\attachments-bbf5c2c761db782a8630d631279f7e13\\';
+$CONFIG['attachment_fspath'] = 'C:\\Inetpub\\wwwroot\\';
 
