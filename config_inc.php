6eb9a91fe2ce640dff60ddd141c5833b2725da51
laguirre70 <leonelaguirre@hotmail.com>
2014-03-14 15:35:14 -0700

    Update config_inc.php
    Added smtp settings

--- config_inc.php
+++ config_inc.php
@@ -4,4 +4,18 @@
 	$g_database_name = 'as_31987ead5571ba2';
 	$g_db_username = 'bc5dce6c699515';
 	$g_db_password = 'eb0f293e';
+
+# --- Email Configuration ---
+$g_phpMailer_method		= PHPMAILER_METHOD_MAIL; # or PHPMAILER_METHOD_SMTP, PHPMAILER_METHOD_SENDMAIL
+$g_smtp_host			= 'smtp.sendgrid.net';			# used with PHPMAILER_METHOD_SMTP
+$g_smtp_username		= 'azure_71fee2a02b1467ba6c036d6994032e85@azure.com';					# used with PHPMAILER_METHOD_SMTP
+$g_smtp_password		= 'hma6ay0v';					# used with PHPMAILER_METHOD_SMTP
+$g_administrator_email  = 'leo@goconcierge.net';
+$g_webmaster_email      = 'leo@goconcierge.net';
+$g_from_email           = 'mantis@goconcierge.net';	# the "From: " field in emails
+$g_return_path_email    = 'leo@goconcierge.net';	# the return address for bounced mail
+# $g_from_name			= 'Mantis Bug Tracker';
+# $g_email_receive_own	= OFF;
+# $g_email_send_using_cronjob = OFF;
+
 ?>
